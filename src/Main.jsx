fa928b1a496fa3388f147c8870eba44b22c17dc2
Joseph Madamba <madamba.joehu@outlook.com>
2025-05-08 23:39:38 -0700

    Simplify key logic

--- src/Main.jsx
+++ src/Main.jsx
@@ -17,7 +17,6 @@ const Main = () => {
   const [playlist, setPlaylist] = React.useState("true");
   const [shuffle, setShuffle] = React.useState(true);
   const [replay, setReplay] = React.useState(true);
-  const [key, setKey] = React.useState(0); //Foreign key for other modes
   const [songList, setSongList] = React.useState([[], []]);
   const [uiVolume, setUiVolume] = React.useState(0.5);
   const [textSize, setTextSize] = React.useState(1);
@@ -60,6 +59,8 @@ const Main = () => {
     }
   };
 
+  const getKey = () => songList[mode - 1].findIndex((x) => x === songIndex + 1);
+
   const changeSong = (e) => {
     //Changes the song using conditions ~from player
     if (mode === 0) {
@@ -91,13 +92,12 @@ const Main = () => {
     ) {
       //Check if array is empty
       if (shuffle === false) {
+        const key = getKey();
         if (e === true) {
           if (key + 1 < songList[mode - 1].length) {
             setIndex(songList[mode - 1][key + 1] - 1);
-            setKey(key + 1);
           } else {
             setIndex(songList[mode - 1][0] - 1);
-            setKey(0);
           }
         } else if (e === false) {
           if (key - 1 < 0) {
@@ -106,10 +106,8 @@ const Main = () => {
               (e) => e.id === tempSong[tempSong.length - 1],
             );
             setIndex(tempId);
-            setKey(tempSong.length - 1);
           } else {
             setIndex(songList[mode - 1][key - 1] - 1);
-            setKey(key - 1);
           }
         }
       } else {
@@ -135,7 +133,7 @@ const Main = () => {
   };
 
   const removeSong = (y) => {
-    songList[y - 1].splice(key, 1);
+    songList[y - 1].splice(getKey(), 1);
     setSongList([...songList]);
     localStorage.setItem("playlistBocchi", JSON.stringify(songList));
   };
@@ -165,13 +163,6 @@ const Main = () => {
       }
     }
   }, [mode]);
-
-  React.useEffect(() => {
-    //Sets key anchor
-    if (mode === 1 || mode === 2) {
-      setKey(songList[mode - 1].findIndex((x) => x === songIndex + 1));
-    }
-  }, [songIndex]);
 
   React.useEffect(() => {
     //Loads and sets data onstart
